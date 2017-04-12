d29a84624c68754098abcbd58b6043117c3e9107
Unknown <nanobot1@hotmail.de>
2017-04-12 22:11:12 +0200

    Init1

---
+++ app/va.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class va extends Model
+{
+    //
+    protected $table = 'VA';
+    protected $primaryKey = 'VAID';
+
+    public $timestamps = false;
+
+
+}
---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Testseite</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+ <h1>Test</h1>
+    <div id="app">
+
+        <test></test>
+    </div>
+ <script src="js/app.js" charset=""></script>
+</html>
